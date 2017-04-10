f91377eb65efffce2c908c32f9828fc544bd4db3
Hamza ESSAYEGH <hamza.essayegh@protonmail.com>
2017-04-10 15:19:07 +0200

    Add username property to the QKey entity

--- Entity/QKey.php
+++ Entity/QKey.php
@@ -19,6 +19,11 @@ class QKey
     /**
      * @var string
      */
+    private $username;
+
+    /**
+     * @var string
+     */
     private $recipient;
 
     /**
@@ -31,11 +36,13 @@ class QKey
      *
      * @param string $recipient
      * @param string $passphrase
+     * @param null   $username
      */
-    public function __construct($recipient = null, $passphrase = null)
+    public function __construct($recipient = null, $passphrase = null, $username = null)
     {
         $this->recipient  = $recipient;
         $this->passphrase = $passphrase;
+        $this->username   = $username;
     }
 
     /**
@@ -92,6 +99,25 @@ class QKey
     public function setRecipient($recipient)
     {
         $this->recipient = $recipient;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     *
+     * @return QKey
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
         return $this;
     }
 }
