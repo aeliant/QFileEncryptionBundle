4c14f04b16339c73d32e20dfb6691dad7e2d5a42
Hamza ESSAYEGH <hamza.essayegh@protonmail.com>
2017-04-12 18:06:01 +0200

    Add specific Exception to ease the handling of errors

---
+++ Exception/KeyGenerationException.php
@@ -0,0 +1,13 @@
+<?php
+namespace Querdos\QFileEncryptionBundle\Exception;
+
+/**
+ * Exception thrown when the key generation has failed
+ *
+ * Class KeyGenerationException
+ * @package Querdos\QFileEncryptionBundle\Exception
+ * @author  Hamza ESSAYEGH <hamza.essayegh@protonmail.com>
+ */
+class KeyGenerationException extends \RuntimeException
+{
+}
---
+++ Exception/KeyImportException.php
@@ -0,0 +1,17 @@
+<?php
+namespace Querdos\QFileEncryptionBundle\Exception;
+
+/**
+ * Class KeyImportException
+ *
+ * Exception thrown an importation failed:
+ *      - Private key import
+ *      - Public key import
+ *
+ * @package Querdos\QFileEncryptionBundle\Exception
+ *
+ * @author  Hamza ESSAYEGH <hamza.essayegh@protonmail.com>
+ */
+class KeyImportException extends \RuntimeException
+{
+}
